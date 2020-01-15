60aef5d66b0fa3cfbf1a01c5166e1b708e1793c7
Matt Daisley <mattdaisley@gmail.com>
2020-01-14 22:41:06 -0700

    Updates

---
+++ precache-manifest.d4a9433d28b2b9206b67625b67314e20.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1289bc0dbcdaec07f9c0a4b8630dee2e",
+    "url": "/battery-calculator-web/index.html"
+  },
+  {
+    "revision": "2d159075749600872795",
+    "url": "/battery-calculator-web/static/css/main.3a863688.chunk.css"
+  },
+  {
+    "revision": "859bdbebafe5b1386306",
+    "url": "/battery-calculator-web/static/js/2.d2ae912d.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/battery-calculator-web/static/js/2.d2ae912d.chunk.js.LICENSE"
+  },
+  {
+    "revision": "2d159075749600872795",
+    "url": "/battery-calculator-web/static/js/main.d3878b44.chunk.js"
+  },
+  {
+    "revision": "b570da5b8895e38dfcec",
+    "url": "/battery-calculator-web/static/js/runtime-main.4540350a.js"
+  }
+]);
